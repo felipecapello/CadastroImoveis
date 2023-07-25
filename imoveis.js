4bcb6cbdfa0fee4775c7109b59b80ac69d045488
Felipe Capello <felipe_capello74@hotmail.com>
2023-07-25 10:15:30 -0300

    Finalizado

--- imoveis.js
+++ imoveis.js
@@ -1,21 +1,30 @@
 imoveis = []
-caracImovel = {}
+imovel = ''
+
 do {
-    let quantidadeImoveis = imoveis.lenght
-    opcao = prompt("Quantidade de imoveis cadastrados: " + (quantidadeImoveis) + "\n \n Escolha uma opção: \n 1 - Cadastrar novo imóvel \n 2 - Mostrar todos os imóveis cadastrados \n 3 - Encerrar programa")
     
+    opcao = prompt("Quantidade de imoveis cadastrados: " + (imoveis.length) + "\n \n Escolha uma opção: \n 1 - Cadastrar novo imóvel \n 2 - Mostrar todos os imóveis cadastrados \n 3 - Encerrar programa")
 
     switch(opcao){
         case "1": 
+         caracImovel = {}
          caracImovel.proprietario = prompt("Digite o nome do proprietario")
          caracImovel.quartos =  prompt("Digite a quantidade de quartos")
          caracImovel.banheiros =  prompt("Digite a quantidade de banheiros")
          caracImovel.garagem =  prompt("Possui garagem?")
-         imoveis += [caracImovel.proprietario + "  " + caracImovel.quartos + " " + caracImovel.banheiros + " " + caracImovel.garagem + "\n"]
+         imoveis.push(caracImovel)
          break
         case "2":
-         alert("Aqui estão todos os imóveis! \n" + imoveis)
-         break
+            for( let i=0;i<imoveis.length;i++){
+              alert( 
+                "Imóvel " + (i+1) +
+                "\nProprietário: " + imoveis[i].proprietario +
+                "\nQuantidade de quartos: " + imoveis[i].quartos +
+                "\nQuantidade de banheiros: " + imoveis[i].banheiros +
+                "\nPossui garagem: " + imoveis[i].garagem
+              )
+            }
+         break    
         case "3":
          alert("Encerrando programa")
          break 
